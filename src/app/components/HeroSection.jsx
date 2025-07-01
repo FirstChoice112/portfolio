5e8fd4f7c96c0a0ad4cdad523e938e20cf408d98
JOHAN\jesus <johan.skoog.1986@gmail.com>
2025-07-01 17:06:45 +0200

    Finished Hero section, and added animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,26 +1,63 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I'm Johan
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-purple-400 to-pink-600">
+              Hello, I'm
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                "Johan",
+                2000,
+                "Web Developer",
+                2000,
+                "Frontend Developer",
+                2000,
+                "Backend Developer",
+                2000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: "1em", display: "inline-block" }}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xm">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
             nemo voluptate nobis mollitia deserunt possimus!
           </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hire me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br  from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </div>
         </div>
-        <div className="col-span-5"></div>
-        <Image
-          src="/images/pathetic.jpg"
-          alt="picture"
-          width={200}
-          height={100}
-        />
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] relative lg:w-[400px] lg:h-[400px]">
+            <Image
+              src="/images/pathetic.jpg"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 rounded-full top-1/2 left-1/2"
+              width={300}
+              height={300}
+              priority
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
