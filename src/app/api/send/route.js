4ba0c28af24b3b6807107cfef73ac903103b17b2
JOHAN\jesus <johan.skoog.1986@gmail.com>
2025-07-03 17:05:35 +0200

    started on Emailsection and route file for resend.com

---
+++ src/app/api/send/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+//Skaffa Domän och lägg den på mitt konto i resend.com för att få det att fungera (1:23:00): https://www.youtube.com/watch?v=Kb1f5bvF6f4
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
+
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
+  try {
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
