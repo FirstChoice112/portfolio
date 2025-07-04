17a8a6c4665c5c4d959873ec72ecdbe218940627
JOHAN\jesus <johan.skoog.1986@gmail.com>
2025-07-04 11:00:37 +0200

    added footer component

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import GithubIcon from "../../../public/images/github-icon.svg";
 import LinkedinIcon from "../../../public/images/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
+  const [emailSubmitted, setEmailSubmittet] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "api/send";
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+    const response = await fetch(endpoint, options);
+    const resData = await response.json();
+    console.log(resData);
+    if (response.status === 200) {
+      console.log("Message sent.");
+    }
+  };
+
   return (
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_#6B21A8,_transparent)] rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
@@ -25,7 +53,7 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <form className="flex flex-col">
+        <form className="flex flex-col" onSubmit={handleSubmit}>
           <div className="mb-6">
             <label
               htmlFor="email"
@@ -34,6 +62,7 @@ const EmailSection = () => {
               Your email
             </label>
             <input
+              name="email"
               type="email"
               id="email"
               required
@@ -49,6 +78,7 @@ const EmailSection = () => {
               Subject
             </label>
             <input
+              name="subject"
               type="text"
               id="subject"
               required
